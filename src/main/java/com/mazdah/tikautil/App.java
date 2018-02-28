241d7355e76a45d3c25dae5af2c74751ed982871
Mazdah <woohj70@gmail.com>
2018-02-28 16:05:54 +0900

    샘플 코드 작성

--- src/main/java/com/mazdah/tikautil/App.java
+++ src/main/java/com/mazdah/tikautil/App.java
@@ -1,5 +1,10 @@
 package com.mazdah.tikautil;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tika.Tika;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+      //assume example.mp3 is in your current directory
+        File file = new File("sample.xls");//
+        
+        //Instantiating tika facade class 
+        Tika tika = new Tika();
+        
+        //detecting the file type using detect method
+        String filetype;
+		try {
+			filetype = tika.detect(file);
+			System.out.println("FILE TYPE = " + filetype);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
